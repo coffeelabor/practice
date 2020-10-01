913c68f8b893b965953e2a0db935ef2849428ef7
Reed James <reedtjames1234@gmail.com>
2020-10-01 11:38:00 -0500

    revert to commit 212b14302fe591a0ad2900ab1cbfe6147680a398

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {Provider} from 'react-redux'
-import {store} from './store'
-
-const AppWithProvider = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppWithProvider />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
