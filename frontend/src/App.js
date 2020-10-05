c3c501be775647bde4676c0ae66263875e1b8d34
Reed James <reedtjames1234@gmail.com>
2020-10-04 22:35:07 -0500

    switching projects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useReducer } from 'react';
 import Todos from './components/Todos'
+import {rootReducer, initialState} from "./reducers/index"
 import './App.css';
 
-class App extends Component {
-  state = {
-    todos: [
-      {
-        id:1,
-        title: 'do some stuff',
-        completed: false
-      },
-      {
-        id:2,
-        title: 'mow yard',
-        completed: false
-      },
-      {
-        id:3,
-        title: 'gutters',
-        completed: false
-      }
-    ]
-  }
-  render(){
+// class App extends Component {
+const App = () => {
+  const [state, dispatch] = useReducer(rootReducer, initialState)
 
-    return (
-      <div className="App">
-        {/* <h1>Howdy world, my old friend</h1> */}
-        <Todos todos={this.state.todos}/>
+
+  return (
+    <div className="App">
+      {/* <h1>Howdy world, my old friend</h1> */}
+      <Todos todos={this.state.todos}/>
     </div>
   );
-}
+
 }
 
 export default App;
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,13 +1,36 @@
 const initialState = {
-    Global: {
-        NewConfirmed: '',
-        TotalConfirmed: ''
-    }
+  todos: [
+    {
+      id: 1,
+      title: "do some stuff",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "mow yard",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "gutters",
+      completed: false,
+    },
+  ],
 };
 
-export const rootReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-
+export const rootReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            const newTodo = {
+                item: action.payload,
+                completed: false,
+                id: Date.now()
+            }
+            return{
+                ...state,
+                todos: [...state.todos, newTodo]
+            }
+        
         default:
             return state;
     }
